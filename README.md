bf768af201181285239f4b2550b650c45c90b40a
Edison Garcia <egachi@live.com>
2021-02-05 19:57:58 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodeJS - Middleware
+
+>**The purpose of this repository is intended just for training material and it is not recommended to take this as a reference for production scenarios.**
---
+++ server.js
@@ -0,0 +1,37 @@
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+function getUsers(){
+    var id = randomInteger(1, 5) 
+    if (id != 3) {
+        var users =  [
+            { "name":randomString(), "lastname": randomString() },
+            { "name":randomString(), "lastname": randomString() },
+            { "name":randomString(), "lastname": randomString() }
+          ]
+          return users;
+    } else {
+        process.exit(1);
+    }   
+}
+
+function randomString() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+app.get('/', function(req, res) {
+    res.json({ users: getUsers() });   
+});
+
+var port = process.env.PORT || 3000;    
+
+app.listen(port, function () {
+    console.log(`Server running at http://localhost:${port}`);
+});
